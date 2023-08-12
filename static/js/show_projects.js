bb23423858212d35b8e5256cb8c1582d1b6c275c
Daniil Ubica <acveloff@gmail.com>
2023-08-12 17:10:17 +0300

    updated

--- static/js/show_projects.js
+++ static/js/show_projects.js
@@ -1,3 +1,12 @@
-function show_text() {
-    document.getElementById('developer').style.display='block';
+function on_proj_over() {
+    document.getElementById("p1").style.cursor = "pointer";
+    document.getElementById("p2").style.cursor = "pointer";
+    document.getElementById("p3").style.cursor = "pointer";
+}
+
+function init_projects() {
+    document.getElementById("p2").style.left = '0%';
+    document.getElementById("p2").style.top = '35%';
+    document.getElementById("p3").style.left = '20%';
+    document.getElementById("p3").style.top = '11%';
 }
